6de50a558363facbf7e558ee127d2d02157d77df
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-01-20 10:46:41 -0700

    working on rendering the post from db.json

--- blog-client/src/components/Post.js
+++ blog-client/src/components/Post.js
@@ -4,9 +4,7 @@ function Post({title, date, description}) {
 
     return (
         <ul>
-            <h3>{title}</h3>
-            <h5>{date}</h5>
-            <p>{description}</p>
+
         </ul>
     )
 }
