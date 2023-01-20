259e6b490ed9fa120804d9c09b0a4385ee32c1d3
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-01-20 11:15:43 -0700

    working on CURD delete

--- blog-client/src/components/Post.js
+++ blog-client/src/components/Post.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-function Post({title, date, description}) {
+function Post({id, title, date, description}) {
+
+    function handleDeleteClick() {
+        fetch(`http://localhost:6001/blogPost/${id}`)
+    }
 
     return (
         <ul>
             <h3>{title}</h3>
             <h5>{date}</h5>
             <p>{description}</p>
+            <button>EDIT</button> <button onClick={handleDeleteClick}>DELETE</button>
         </ul>
     )
 }
