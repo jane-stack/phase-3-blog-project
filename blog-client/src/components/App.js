aab001c7fb5996828bb5085366090d3e5e60c88a
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-01-20 11:04:56 -0700

    Got the POST method working. Able to post a new Blog post and have it render immmediately onto the page.

--- blog-client/src/components/App.js
+++ blog-client/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import BlogList from "./BlogList";
+import NewPost from "./NewPost";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -8,12 +9,17 @@ function App() {
   useEffect(() => {
     fetch("http://localhost:6001/blogPost")
     .then(resp => resp.json())
-    .then(posts  => setPosts(posts))
-  }, [])
+    .then(posts  => setPosts(posts));
+  }, []);
+
+  const handleAddPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  }
 
   return (
     <main className="App">
-      <Header/>
+      <Header />
+      <NewPost onAddPost={handleAddPost} />
       <BlogList posts={posts} />
     </main>
   );
