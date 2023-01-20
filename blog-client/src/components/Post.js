07778836a49e78e3ae20c60c616ae0f78833191a
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-01-20 10:47:11 -0700

    rendered the post.

--- blog-client/src/components/Post.js
+++ blog-client/src/components/Post.js
@@ -4,7 +4,9 @@ function Post({title, date, description}) {
 
     return (
         <ul>
-
+            <h3>{title}</h3>
+            <h5>{date}</h5>
+            <p>{description}</p>
         </ul>
     )
 }
